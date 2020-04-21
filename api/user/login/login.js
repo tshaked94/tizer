b9843b027c4d94963ab6a439600061ae83d09164
Sahar Hagbi <noamsahar17@gmail.com>
2020-04-21 13:35:53 +0300

    commit for tomer

--- api/user/login/login.js
+++ api/user/login/login.js
@@ -1,10 +1,35 @@
 const router = require('../../../server').express.Router();
 const login = require('../../../lib/model/user/login');
+const user = require('../../../lib/database/schemas/User');
 
-router.get('/', (request, response) => {
-    token = request.body.activeToken;
-    var profile = login(token);
-    response.send(profile);
+router.get('/', async (request, response) => {
+
+    console.log('in api/user/login');
+
+    const { id, token, email, name } = request.body;
+
+    // console.log('first name is:' + firstName);
+    // console.log('last name is:' + lastName);
+
+    let newUser = {
+        id: id,
+        token: token,
+        email: email,
+        name: name
+    };
+    console.log('new user after init is: ' + newUser.toString());
+
+    let userModel = new user(newUser);
+
+    console.log('new user is ' + userModel.toString());
+
+    // await userModel.save().catch(() => console.log('user has not been saved'));
+    userModel.save();
+    response.json(userModel);
+
+    // token = request.body.activeToken;
+    // var profile = login(token);
+    // response.send(profile);
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const app = require('./server').app
+const express = require('express');
 const bodyParser = require("body-parser");
 const api = require('./api/api');
+const connector = require('./lib/database/connection');
 
+// const mongoose = require('mongoose');
+// const keys = require('./lib/config/keys');
+
+
+connector.connectDB();
+
+// mongoose.connect(keys.mongodb.dbURI,
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     () => {
+//         console.log('Connected to MongoDB');
+//     });
+
+app.use(express.json({ extended: false }));
 app.use(
     bodyParser.urlencoded({
         extended: true
@@ -10,9 +25,11 @@ app.use(
 
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('hi');
 });
+
+
 
 app.use('/api', api);
 
