09886c4a7177e3c92204280ac7cece5f11134abb
Sahar Hagbi <noamsahar17@gmail.com>
2020-05-02 17:30:49 +0300

    Co-authored-by: Tomer Shaked <tomersd@mta.ac.il>

--- api/user/login/login.js
+++ api/user/login/login.js
@@ -5,18 +5,26 @@ const User = require('../../../lib/database/schemas/User');
 router.get('/tizer', async (request, response) => {
 
     console.log('in api/user/login/tizer');
-    userLogin.checkToken(request.get('token'), function(result){
-        console.log(result);
-        response.send(result);
-    });
-
+    userLogin.checkToken(request.get('token'))
+        .then((result) => {
+            console.log('in result');
+            console.log(result);
+            response.send(result);
+        })
 });
 
 router.get('/facebook', async (request, response) => {
     console.log('in api/user/login/facebook');
-    await userLogin.login(request.get('access_token'), function(token){
-        response.send(token);
-    });
+    console.log(request.get('access_token'));
+
+    await userLogin.login(request.get('access_token'))
+        .then((token) => {
+            console.log('in cb func');
+            response.send(token);
+        }).catch((error) => {
+            console.log(error.message)
+            response.send(error);
+        });
 });
 
 module.exports = router;
