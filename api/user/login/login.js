67c08e192173a3a5d52815eacc9aca0bb682a1b8
Sahar Hagbi <noamsahar17@gmail.com>
2020-06-08 22:52:09 +0300

    commit for heroku

--- api/user/login/login.js
+++ api/user/login/login.js
@@ -5,29 +5,31 @@ const User = require('../../../lib/database/schemas/user/User');
 router.get('/tizer', async (request, response) => {
 
     console.log('in api/user/login/tizer');
-    response.send('its still you');
-    // userLogin.checkToken(request.get('token'))
-    //     .then((result) => {
-    //         console.log('in result');
-    //         console.log(result);
-    //         response.send(result);
-    //     })
+    
+    userLogin.checkToken(request.get('token'))
+        .then((result) => {
+            console.log('in result');
+            console.log(result);
+            response.send(result);
+        })
 });
 
 router.get('/facebook', async (request, response) => {
     console.log('in api/user/login/facebook');
-    //console.log(request.get('access_token'));
+    const accessToken = request.query.access_token;
 
-    response.send(1593620856209);
+    console.log(accessToken);
 
-    // await userLogin.login(request.get('access_token'))
-    //     .then((token) => {
-    //         console.log('in cb func');
-    //         response.send(token);
-    //     })
-    //     .catch((err) => {
-    //         console.log('in catch login facenook block');
-    //     });
+    await userLogin.login(accessToken)
+        .then((token) => {
+            console.log('in cb func');
+            response.send(token);
+        })
+        .catch((err) => {
+            console.log('in catch login facenook block');
+            console.log('error:');
+            // console.log(err);
+        });
 });
 
 module.exports = router;
