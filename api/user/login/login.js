c9b7375d2378398b69391d51b7d3fbc60ed788c5
Tomer Shaked <tomershaked22@gmail.com>
2020-06-09 16:31:46 +0300

    login flow done

--- api/user/login/login.js
+++ api/user/login/login.js
@@ -1,6 +1,7 @@
 const router = require('../../../server').express.Router();
 const userLogin = require('../../../lib/model/user/login');
 const User = require('../../../lib/database/schemas/user/User');
+const date = require('../../../lib/model/utils/date');
 
 router.get('/tizer', async (request, response) => {
     console.log('in api/user/login/tizer');
@@ -9,10 +10,13 @@ router.get('/tizer', async (request, response) => {
             console.log('in result');
             console.log(result);
             response.send({ user: result });
+        }).catch((err) => {
+            response.status(400).send(err.message);
         })
 });
 
 router.get('/facebook', async (request, response) => {
+    var time = date.getDateTime(date.timeUnits.month, 2);
     await userLogin.login(request.get('accessToken'))
         .then((token) => {
             console.log('in cb func');
@@ -20,7 +24,7 @@ router.get('/facebook', async (request, response) => {
         })
         .catch((err) => {
             console.log('in error func');
-            response.status(400).send(err.message);
+            response.status(400).send({error: err.message});
         });
 });
 module.exports = router;
