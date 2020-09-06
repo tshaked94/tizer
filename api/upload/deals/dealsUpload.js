0731dff79a30bde96da1eeb9fadf16de6970a8de
Tomer Shaked <tomershaked22@gmail.com>
2020-09-06 21:15:04 +0300

    return deal upload photo to normal

--- api/upload/deals/dealsUpload.js
+++ api/upload/deals/dealsUpload.js
@@ -4,9 +4,8 @@ const { uploadPhoto } = require('../../../lib/model/modelController');
 
 router.post('/:id', async (request, response) => { // upload photo for specific store
     console.log('in upload deal photo');
-    response.send('in upload deal photo!');
-    // const { id } = request.params;
-    // uploadPhoto(id, request, response, "deal");
+    const { id } = request.params;
+    uploadPhoto(id, request, response, "deal");
 });
 
 module.exports = router;
