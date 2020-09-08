b7387dd4e87b6f1027dfb36f167141ebdf89d58f
Tomer Shaked <tomershaked22@gmail.com>
2020-09-08 23:44:20 +0300

    test

--- api/upload/deals/dealsUpload.js
+++ api/upload/deals/dealsUpload.js
@@ -4,6 +4,7 @@ const { uploadPhoto } = require('../../../lib/model/modelController');
 
 router.post('/:id', async (request, response) => { // upload photo for specific store
     console.log('in upload deal photo');
+    response.send('in upload deal photo');
     const { id } = request.params;
     const { image } = request.body;
     link = await uploadPhoto(id, image, 'deal');
