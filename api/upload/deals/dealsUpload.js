baccad16da351b74ca01c5a5e853fc17829dad95
Tomer Shaked <tomershaked22@gmail.com>
2020-09-08 23:52:55 +0300

    revert

--- api/upload/deals/dealsUpload.js
+++ api/upload/deals/dealsUpload.js
@@ -4,11 +4,10 @@ const { uploadPhoto } = require('../../../lib/model/modelController');
 
 router.post('/:id', async (request, response) => { // upload photo for specific store
     console.log('in upload deal photo');
-    response.send('in upload deal photo');
-    // const { id } = request.params;
-    // const { image } = request.body;
-    // link = await uploadPhoto(id, image, 'deal');
-    // response.send(link);
+    const { id } = request.params;
+    const { image } = request.body;
+    link = await uploadPhoto(id, image, 'deal');
+    response.send(link);
 });
 
 module.exports = router;
