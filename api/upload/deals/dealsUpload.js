b2b6e98805d4735b7bba3e34b8ec0d51782436cc
Sahar Hagbi <noamsahar17@gmail.com>
2020-09-10 15:29:08 +0300

    pushing for debug uploadPhoto

--- api/upload/deals/dealsUpload.js
+++ api/upload/deals/dealsUpload.js
@@ -4,10 +4,13 @@ const { uploadPhoto } = require('../../../lib/model/modelController');
 
 router.post('/:id', async (request, response) => { // upload photo for specific store
     console.log('in upload deal photo');
+    console.log(request);
+    console.log('===============================================');
     const { id } = request.params;
     const { image } = request.body;
     link = await uploadPhoto(id, image, 'deal');
     response.send(link);
+
 });
 
 module.exports = router;
