33417b2ba50c97db7ea47fb19e99ed4a64aa3556
Sahar Hagbi <noamsahar17@gmail.com>
2020-09-14 22:57:28 +0300

    fixing login - new users

--- database/utils/time.js
+++ database/utils/time.js
@@ -8,7 +8,35 @@ const setTimeSinceEpoch = (object) => {
         object.expiration_date = Math.floor(new Date(expiration_date).getTime() / 1000);
 }
 
+const timeUnits = {
+    day: 'day',
+    month: 'month',
+    year: 'year'
+}
+Object.freeze(timeUnits);
+
+const getDateTime = (time, amount) => {
+    var date = new Date();
+    switch (time) {
+        case timeUnits.day:
+            date.setDate(date.getDate() + amount);
+            break;
+        case timeUnits.month:
+            date.setMonth(date.getMonth() + amount);
+            break;
+        case timeUnits.year:
+            date.setFullYear(date.getFullYear() + amount);
+            break;
+        default:
+            date = null;
+    }
+    return Math.floor(date.getTime() / 1000);
+}
+
+
 module.exports = {
     setTimeSinceEpoch,
-    getCurrentTime24Format
+    getCurrentTime24Format,
+    timeUnits,
+    getDateTime
 }
